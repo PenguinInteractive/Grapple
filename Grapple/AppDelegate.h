0d56f4bb78c0aa2df0308688f2bcdefac6df5c67
Colt King <coltking@Colts-iMac.local>
2018-02-16 21:51:13 -0800

    Initial Commit

---
+++ Grapple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Grapple
+//
+//  Created by Colt King on 2018-02-16.
+//  Copyright © 2018 Penguin Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Grapple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Grapple
+//
+//  Created by Colt King on 2018-02-16.
+//  Copyright © 2018 Penguin Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
