5a4ef622d9475fe058820e6dcc6661c822fe63c3
Unknown <blizzardkang@gmail.com>
2018-02-25 03:15:10 -0800

    my Generate function for the platforms

---
+++ Grapple/Generator.m
@@ -0,0 +1,9 @@
+//
+//  Generator.m
+//  Grapple
+//
+//  Created by BCIT Student on 2018-02-25.
+//  Copyright © 2018 Penguin Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
